bafcef79435abaeb396e7d290c3109a9cf349b5d
Jasmine Lin <jaslin@berkeley.edu>
2023-04-23 23:19:03 -0700

    saving progress

---
+++ final/assets/js/links.js
@@ -0,0 +1,35 @@
+// For clicking to history pages and recipe pages. 
+// Black milk tea
+// function history1() {
+//     window.open('black_milk_tea.html'); 
+// }
+// document.getElementById('black-milk-tea-history').onclick = history1; 
+
+
+// // Thai milk tea 
+// function history2() {
+//     window.replace('https://thaiginger.com/sip-on-this-the-fascinating-history-of-thai-iced-tea/', '_thai_history');
+// }
+// document.getElementById('thai-milk-tea-history').onclick = history2; 
+
+
+// // Taro milk tea 
+// function history3() {
+//     window.location.href = 'https://www.leaf.tv/articles/what-is-taro-bubble-tea/';
+// }
+// document.getElementById('taro-milk-tea-history').onclick = history3; 
+
+
+// // Matcha milk tea 
+// function history4() {
+//     https://www.matchaful.com/pages/the-history-of-matcha
+// }
+// document.getElementById('matcha-milk-tea-history').onclick = history4; 
+
+
+// // Strawberry milk tea 
+// function history5() {
+//     https://teabackyard.com/strawberry-milk-tea/
+// }
+// document.getElementById('strawberry-milk-tea-history').onclick = history5; 
+
